e383851f5ca9a04ecaba9c195ea797829fd2e7c5
Mane Motha <motha.manelisi@gmail.com>
2022-04-23 19:27:24 +0200

    Log in: remove password but leave profiles inside a list for iteration.

--- logic/user/room.py
+++ logic/user/room.py
@@ -4,20 +4,14 @@ from .__init__ import *
 # application user
 class Rooms:
 
-    def __init__(self, user: dict, room_content: dict):
-        self.user: dict = user
-        self.room: dict = room_content
-        self.id: str = room_content['id']
-        self.room_author: str = room_content['author']
-
-        # user "rooms" database instance
-        self.database = TinyDB(f'{self.user["email"]}').table('rooms')
+    def __init__(self):
+        pass
 
     def new_room(self):
-        self.database.insert(self.room)
+        pass
 
     def delete_room(self):
-        self.database.remove(Query().matches())
+        pass
 
     def update_room(self):
         pass
