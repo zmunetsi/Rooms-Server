262cfb266e4acd601cd5dbad38d24a782e2d37a2
Mane Motha <motha.manelisi@gmail.com>
2022-05-06 15:36:10 +0200

    Ensure username is lowercase

--- logic/account/account.py
+++ logic/account/account.py
@@ -18,7 +18,7 @@ class RoomAccount:
         self.profile: dict = profile  # user's full profile
         self.email: str = profile['email']
         self.password: str = profile['password']
-        self.username: str = profile['username']
+        self.username: str = profile['username'].lower()  # lower case
 
         # current work directory
         self.account_directory: str = f'system/user/account/{self.username}'
